4f7fa2c38464dc3a7e4a85933ccea9f287abd511
Andrew <andrewmocol@hotmail.com>
2021-02-24 21:24:30 -0600

    key

--- Kin.Base/models/Key.cs
+++ Kin.Base/models/Key.cs
@@ -1,4 +1,5 @@
 ﻿using Kin.Base.tools;
+using Kin.Stellar.Sdk;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -35,18 +36,76 @@ namespace Kin.Base.models
 
             public string stellarBase32Encode()
             {
-                return toKeyPair().accountId;
+                return KeyPair.FromPublicKey(value).AccountId;
             }
 
             public string base58Encode()
             {
                 return Base58.encode(value);
             }
+
+            public bool verify(byte[] data, byte[] value)
+            {
+                return KeyPair.FromPublicKey(this.value).Verify(data, value);
+            }
+
+            public static byte[] decode(string _value)
+            {
+                return KeyPair.FromAccountId(_value).PublicKey;
+            } 
         }
 
         public class PrivateKey
         {
-            public PrivateKey() { }
+            private byte[] value;
+            public PrivateKey(string privateKeyString) 
+            {
+                value = Encoding.ASCII.GetBytes(KeyPair.FromSecretSeed(privateKeyString).SecretSeed);
+            }
+
+            public bool equals(object other)
+            {
+                if (this == other) return true;
+
+                other = other as PrivateKey;
+                if (!value.Equals(other)) return false;
+
+                return true;
+            }
+            public int hashCode()
+            {
+                return value.GetHashCode();
+            }
+
+            public string toString()
+            {
+                return "Key.PrivateKey(value=XXXXXXXX<Private>XXXXXXXX)";
+            }
+
+            public string stellarBase32Encode()
+            {
+                return String.Format(KeyPair.FromSecretSeed(value).SecretSeed);
+            }
+
+            public string base58Encode()
+            {
+                return Base58.encode(value);
+            }
+
+            public byte[] sign(byte[] data)
+            {
+                return KeyPair.FromSecretSeed(value).Sign(data);
+            }
+
+            public static byte[] decode(string _value)
+            {
+                return KeyPair.FromAccountId(_value).PrivateKey;
+            }
+
+            public static PrivateKey random()
+            {
+                return new PrivateKey(KeyPair.Random().SecretSeed);
+            }
         }
     }
 }
